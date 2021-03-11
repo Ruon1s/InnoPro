b41fec3cc70b32f6c6411033b58b81757211bf7f
Ruon1s <50231327+Ruon1s@users.noreply.github.com>
2021-03-11 17:04:29 +0200

    'firebaseconffed'

--- App.tsx
+++ App.tsx
@@ -2,12 +2,19 @@ import React, {useEffect} from 'react';
 import TabNavigator from './src/navigators/TabNavigator';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
+import firebase from "firebase/app";
+import "firebase/firestore"
+import {firebaseConfig} from "./src/utils/firebaseConfig";
 import {fetchWeather} from "./src/store/weather/actions";
 import { useDispatch } from 'react-redux';
 import {getLocation} from "./src/store/location/actions";
 
-const App = () => {
+if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+    console.log('firebase initialized')
+}
 
+const App = () => {
   return (
     <Provider store={store}>
       <TabNavigator />
