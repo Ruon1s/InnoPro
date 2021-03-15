afbdeff2419f5b8fbe570365b5b343e14be7cf04
laurinis <lauri.nissinen@metropolia.fi>
2021-03-15 16:09:55 +0200

    Sum work

--- src/store/transportation/actions.ts
+++ src/store/transportation/actions.ts
@@ -2,17 +2,41 @@ import {Action} from "redux";
 import {ThunkAction} from "redux-thunk";
 import {RootState} from "..";
 import {FETCH_TRANSPORT} from "./types";
-import {useQuery,gql} from "@apollo/client";
+import {ApolloClient, InMemoryCache, gql} from '@apollo/client';
 
+const client = new ApolloClient({
+    uri: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+    cache: new InMemoryCache()
+});
 
 export const fetchTransport = (
-    lat: number,
-    lon: number
+    latitude: number,
+    longitude: number
 ): ThunkAction<void, RootState, unknown, Action<string>> => async dispatch => {
     try {
-        const endpoint = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql';
-        const NEAREST_STATION_QUERY = gql;
-
+        const query = gql`
+                    query GetStops($lati: int, $long: int){ 
+                      stopsByRadius(lat: $lati, lon: $long, radius: 250) {
+                        edges{
+                            node{
+                                stop{
+                                    gtfsId
+                                    name
+                                }
+                                distance
+                            }
+                        }
+                      }
+                    }
+                `;
+        client.query({
+            query: query,
+            variables: {
+                lati: latitude,
+                long: longitude
+            }
+        })
+            .then(result => console.log("DATAAA: " + JSON.stringify(result.data)));
 
         const toJSON = "await nearestStations";
 
