b747ff017d672211e678529d738b157061746885
Ruon1s <50231327+Ruon1s@users.noreply.github.com>
2021-03-17 02:34:03 +0200

    merge conflicts

--- src/store/weather/actions.ts
+++ src/store/weather/actions.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Action } from "redux";
 import { ThunkAction } from "redux-thunk";
 import { RootState } from "..";
@@ -7,7 +6,7 @@ import Constants from 'expo-constants';
 import { setErrorMessage, toggleLoading } from "../app/actions";
 
 export const fetchWeather = (
-  lat: number, 
+  lat: number,
   lon: number
 ): ThunkAction<void, RootState, unknown, Action<string>> => async dispatch => {
   try {
@@ -27,23 +26,3 @@ export const fetchWeather = (
     }, 3000);
   }
 }
-=======
-import {Action} from "redux";
-import {ThunkAction} from "redux-thunk";
-import {RootState} from "..";
-import {FETCH_WEATHER} from "./types";
-
-export const fetchWeather = (
-    lat?: number,
-    lon?: number
-): ThunkAction<void, RootState, unknown, Action<string>> => async dispatch => {
-    try {
-        const currentWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat ? lat : '60.205490'}&lon=${lon ? lon : '24.655899'}&units=metric&appid=8c77f2fc166c90f4464bc9df8e6b73cd`);
-        const toJSON = await currentWeather.json();
-
-        dispatch({type: FETCH_WEATHER, payload: toJSON});
-    } catch (error) {
-        console.log(`Fetch Weather error: ${error.message}`);
-    }
-};
->>>>>>> lauri
